77344cb1ddbf774fdb955fbcc4e25a3a561d815d
leofs94 <leofs_94@hotmail.com>
2020-09-08 14:25:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -253,7 +253,7 @@ def ParsearAlerta(alerta):
       estado_servicio
   )
   mensaje = variables_OMI[alerta.labels.alertname]['mensaje']
-  indicaciones = variables_OMI[alerta.labels.alertname]['indicaciones'] + alerta.labels.instance
+  indicaciones = variables_OMI[alerta.labels.alertname]['indicaciones'] + " instance:" +alerta.labels.instance
   componente = variables_OMI[alerta.labels.alertname]['componente']
   if alerta.status == 'resolved':
     estado = "CESE"
