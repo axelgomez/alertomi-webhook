0d83a1e30466f7d3dd2c3eb9c24994c0fe80b160
Axel Gomez <agomez@dblandit.com>
2020-09-30 20:20:01 -0300

    cambios menores

--- main.py
+++ main.py
@@ -343,7 +343,7 @@ def ParsearAlerta(alerta):
         mensaje_recibido += "{}".format(alerta.startsAt)
         mensaje_enviado += "{}".format(alerta.startsAt)
     mensaje_recibido += " - Recibido: {{{}}}".format(alerta)
-    mensaje_enviado += " {}|{}|{}|{}|{}|{}".format(aplicacion,
+    mensaje_enviado += " - Enviado: {}|{}|{}|{}|{}|{}".format(aplicacion,
                                                   titulo,
                                                   mensaje,
                                                   estado,
